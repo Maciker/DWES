4a65ec09153dc941b840f9463f00f9f5aa40f2c5
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2019-05-06 22:17:23 +0200

    Add files via upload

---
+++ 07_Tarea/DB.php
@@ -0,0 +1,102 @@
+<?php 
+class DB {
+    protected static function ejecutaConsulta($sql) {
+        $opc = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+        $dsn = "mysql:host=localhost;dbname=tarea5";
+        $usuario = 'dwes';
+        $contrasena = 'abc123.';
+        
+        $dwes = new PDO($dsn, $usuario, $contrasena, $opc);
+        $resultado = null;
+        if (isset($dwes)) $resultado = $dwes->query($sql);
+        return $resultado;
+    }
+    public static function obtieneProductos() {
+        $sql = "SELECT cod, nombre_corto, nombre, PVP, descripcion FROM producto;";
+        $resultado = self::ejecutaConsulta ($sql);
+        $productos = array();
+	if($resultado) {
+            // Añadimos un elemento por cada producto obtenido
+            $row = $resultado->fetch();
+            while ($row != null) {
+                $productos[] = new Producto($row);
+                $row = $resultado->fetch();
+            }
+	}
+        
+        return $productos;
+    }
+    
+    public static function obtieneProducto($codigo) {
+        $sql = "SELECT cod, nombre_corto, nombre, PVP, descripcion FROM producto";
+        $sql .= " WHERE cod='" . $codigo . "'";
+        $resultado = self::ejecutaConsulta ($sql);
+        $producto = null;
+	if(isset($resultado)) {
+            $row = $resultado->fetch();
+            $producto = new Producto($row);
+	}
+        
+        return $producto;    
+    }
+    
+    public static function obtieneOrdenadores() {
+        $sql = "SELECT cod, disco, grafica, otros, procesador, RAM, SO, unidadoptica FROM ordenador";
+        $resultado = self::ejecutaConsulta ($sql);
+        $ordenadores = array();
+	if($resultado) {
+            // Añadimos un elemento por cada producto obtenido
+            $row = $resultado->fetch();
+            while ($row != null) {
+                $ordenadores[] = new Ordenador($row);
+                $row = $resultado->fetch();
+            }
+	}
+            return $ordenadores;    
+    }
+    
+        public static function codigopcs() {
+        $sql = "SELECT cod FROM ordenador";
+        $resultado = self::ejecutaConsulta ($sql);
+        $codigos = array();
+	if($resultado) {
+            // Añadimos un elemento por cada codigo obtenido
+            $row = $resultado->fetch();
+            while ($row != null) {
+                $codigos[] = $row;
+                $row = $resultado->fetch();
+            }
+	}
+            return $codigos;    
+    }
+        
+       
+        public static function infoordenador($codigo) {
+        $sql = "SELECT cod, disco, grafica, otros, procesador, RAM, SO, unidadoptica FROM ordenador";
+        $sql .= " WHERE cod='" . $codigo . "'";
+        $resultado = self::ejecutaConsulta ($sql);
+        $ordenadores = null;
+        
+	if(isset($resultado)) {
+            $row = $resultado->fetch();
+            $ordenador = new Ordenador($row);
+	}
+        
+        return $ordenador;    
+    }
+    
+    public static function verificaCliente($nombre, $contrasena) {
+        $sql = "SELECT usuario FROM usuarios ";
+        $sql .= "WHERE usuario='$nombre' ";
+        $sql .= "AND contrasena='" . md5($contrasena) . "';";
+        $resultado = self::ejecutaConsulta ($sql);
+        $verificado = false;
+        if(isset($resultado)) {
+            $fila = $resultado->fetch();
+            if($fila !== false) $verificado=true;
+        }
+        return $verificado;
+    }
+    
+}
+?>
---
+++ 07_Tarea/busqueda.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Búsqueda datos de empresa</title>
+<link href="tienda.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <input class="logout" type="button" value="Salir"/>
+    <div id="search">
+    <form action="" method="post">
+    <fieldset >
+        <legend>Búsqueda</legend>
+        <div class="campo">
+            <label for="busqueda">Nombre a buscar:</label><br/>
+            <input type="text" name="busqueda" id="busqueda" placeholder="Escriba nombre o apellido" /><br/>        
+        <div class="campo">
+            <input type="submit" name="Buscar" value="Buscar" />
+        </div>  
+        
+    </fieldset>
+    </form>
+    </div>
+</body>
+</html>
